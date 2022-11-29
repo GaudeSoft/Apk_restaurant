ee67e9f95d580fdece08c6b9ba55ac72f519398c
GaudeSoft <78844968+GaudeSoft@users.noreply.github.com>
2022-11-29 16:48:17 +0100

    Mise en place de la fonctionnalité 'ajout dans le panier'

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View} from 'react-native';
 // import HomeScreen from "./src/screens/HomeScreen"
-import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
+// import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
+// import DishDetailsScreen from './src/screens/DishDetailsScreen';
+import Basket from './src/screens/Basket';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailsScreen />
       {/* <HomeScreen /> */}
-
+      {/* <RestaurantDetailsScreen /> */}
+      {/* <DishDetailsScreen /> */}
+      <Basket />
+      
       <StatusBar style="light" />
     </View>
   );
@@ -19,8 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    // padding: 10,
-    // paddingVertical: 30,
     justifyContent: 'center',
   },
 });
