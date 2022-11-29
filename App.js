7964396bbd19a9893beac3b7c2bf8f96a595349e
GaudeSoft <78844968+GaudeSoft@users.noreply.github.com>
2022-11-29 11:39:39 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+// import HomeScreen from "./src/screens/HomeScreen"
+import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RestaurantDetailsScreen />
+      {/* <HomeScreen /> */}
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -15,6 +19,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    // padding: 10,
+    // paddingVertical: 30,
     justifyContent: 'center',
   },
 });
